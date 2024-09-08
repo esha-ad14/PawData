d29d4e4160e85a88a60489ef49663612109db8de
Esha Adhawade <90581230+esha-ad14@users.noreply.github.com>
2024-09-07 22:52:14 -0700

    frontend code

---
+++ Frontend/tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          10: "#9a805a",
+          20: "#5f4828",
+          30: "#373B39",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
